3366d0f30e4d127aae1cbdbc66e1c3b62ac8e484
hampen2929 <mochi.no.kimochi.2929@gmail.com>
2022-03-06 14:27:01 +0900

    lint

--- util/metric/metric.py
+++ util/metric/metric.py
@@ -2,6 +2,7 @@ import functools
 import os
 import time
 from collections import defaultdict, deque
+from typing import Optional, Tuple
 
 import numpy as np
 import torch
@@ -15,14 +16,14 @@ __all__ = [
 ]
 
 
-def get_total_and_free_memory_in_Mb(cuda_device):
+def get_total_and_free_memory_in_Mb(cuda_device: str) -> Tuple[int, int]:
     devices_info_str = os.popen("nvidia-smi --query-gpu=memory.total,memory.used --format=csv,nounits,noheader")
     devices_info = devices_info_str.read().strip().split("\n")
     total, used = devices_info[int(cuda_device)].split(",")
     return int(total), int(used)
 
 
-def occupy_mem(cuda_device, mem_ratio=0.9):
+def occupy_mem(cuda_device: str, mem_ratio: float = 0.9) -> None:
     """
     pre-allocate gpu memory for training to avoid memory Fragmentation.
     """
@@ -34,7 +35,7 @@ def occupy_mem(cuda_device, mem_ratio=0.9):
     time.sleep(5)
 
 
-def gpu_mem_usage():
+def gpu_mem_usage() -> int:
     """
     Compute the GPU memory usage for the current device (MB).
     """
@@ -47,12 +48,12 @@ class AverageMeter:
     window or the global series average.
     """
 
-    def __init__(self, window_size=50):
+    def __init__(self, window_size: int = 50) -> None:
         self._deque = deque(maxlen=window_size)
         self._total = 0.0
         self._count = 0
 
-    def update(self, value):
+    def update(self, value: float) -> None:
         self._deque.append(value)
         self._count += 1
         self._total += value
@@ -63,29 +64,29 @@ class AverageMeter:
         return np.median(d)
 
     @property
-    def avg(self):
+    def avg(self) -> float:
         # if deque is empty, nan will be returned.
         d = np.array(list(self._deque))
         return d.mean()
 
     @property
-    def global_avg(self):
+    def global_avg(self) -> float:
         return self._total / max(self._count, 1e-5)
 
     @property
-    def latest(self):
+    def latest(self) -> deque:
         return self._deque[-1] if len(self._deque) > 0 else None
 
     @property
-    def total(self):
+    def total(self) -> float:
         return self._total
 
-    def reset(self):
+    def reset(self) -> None:
         self._deque.clear()
         self._total = 0.0
         self._count = 0
 
-    def clear(self):
+    def clear(self) -> None:
         self._deque.clear()
 
 
@@ -96,14 +97,14 @@ class MeterBuffer(defaultdict):
         factory = functools.partial(AverageMeter, window_size=window_size)
         super().__init__(factory)
 
-    def reset(self):
+    def reset(self) -> None:
         for v in self.values():
             v.reset()
 
-    def get_filtered_meter(self, filter_key="time"):
+    def get_filtered_meter(self, filter_key: str = "time"):
         return {k: v for k, v in self.items() if filter_key in k}
 
-    def update(self, values=None, **kwargs):
+    def update(self, values: Optional[float] = None, **kwargs) -> None:
         if values is None:
             values = {}
         values.update(kwargs)
@@ -112,6 +113,6 @@ class MeterBuffer(defaultdict):
                 v = v.detach()
             self[k].update(v)
 
-    def clear_meters(self):
+    def clear_meters(self) -> None:
         for v in self.values():
             v.clear()
